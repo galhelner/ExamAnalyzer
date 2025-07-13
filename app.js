a7eb74bd5e78606f004056104a3acae558b36805
Gal Helner <galhelner@gmail.com>
2025-07-13 11:31:34 +0300

    added basic boiler plate code

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));

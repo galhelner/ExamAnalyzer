32e00755f50734cbdd82c15cb51af18eb8dfd76e
Gal Helner <galhelner@gmail.com>
2025-08-05 10:23:48 +0300

    Add points field to exam schema and remove unused exams array from user schema; update CSS for improved layout

--- models/Exam.js
+++ models/Exam.js
@@ -6,6 +6,7 @@ const examSchema = new mongoose.Schema({
         {
             description: { type: String, required: true },
             options: { type: [String], required: true },
+            points: { type: Number, required: true },
         }
     ],
     createdAt: { type: Date, default: Date.now },
--- models/User.js
+++ models/User.js
@@ -5,11 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['student', 'teacher'], default: 'student' },
-  exams: [
-    {
-      examId: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam' }
-    }
-  ],
 });
 
 module.exports = mongoose.model('User', userSchema);
